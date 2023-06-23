65d534506f26c6eae734e2c99da9bac0c9df9117
Hans Kurniawan <hans.kurniawan@bukalapak.com>
2023-06-23 08:28:15 +0700

    Add print HALO

--- src/latihanalpro2/LatihanAlpro2.java
+++ src/latihanalpro2/LatihanAlpro2.java
@@ -15,6 +15,7 @@ public class LatihanAlpro2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("HALO");
         // TODO code application logic here
     }
     
